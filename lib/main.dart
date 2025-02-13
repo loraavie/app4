48575840f5eb42a5da4262850fcee76186360361
Lora Elliott <128410600+loraavie@users.noreply.github.com>
2025-02-12 21:07:18 -0600

    added some images

--- lib/main.dart
+++ lib/main.dart
@@ -20,39 +20,52 @@ class MyApp extends StatelessWidget {
 class HomeScreen extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
-    return Scaffold(
-      appBar: AppBar(title: Text('Lora\'s Screen')),
-      body: Column(
-        children: [
-          Padding(
-            padding: const EdgeInsets.all(16.0),
-            child: Text('Welcome to Lora\'s Screen',
-                style: TextStyle(fontSize: 20)),
-          ),
-          Expanded(child: Container()),
-          Padding(
-            padding: const EdgeInsets.all(16.0),
-            child: SizedBox(
-              width: 200,
-              height: 50,
-              child: ElevatedButton(
-                style: const ButtonStyle(
-                    backgroundColor:
-                        WidgetStatePropertyAll(Colors.purpleAccent)),
-                onPressed: () {
-                  Navigator.push(
-                    context,
-                    MaterialPageRoute(builder: (context) => ScreenOne()),
-                  );
-                },
-                child: Text(
-                  'Go to Kaleb\'s Screen',
-                  selectionColor: Colors.white,
+    return Container(
+      decoration: BoxDecoration(
+        image: DecorationImage(
+          image: AssetImage('assets/images/bg2.jpg'),
+          fit: BoxFit.cover,
+        ),
+      ),
+      child: Scaffold(
+        backgroundColor: Colors.transparent,
+        appBar: AppBar(title: Text('Lora\'s Screen')),
+        body: Column(
+          children: [
+            Padding(
+              padding: const EdgeInsets.all(16.0),
+              child: Text('Welcome to Lora\'s Screen',
+                  style: TextStyle(fontSize: 20)),
+            ),
+            Image.asset(
+              'assets/images/dog.webp',
+              height: 400,
+            ),
+            Expanded(child: Container()),
+            Padding(
+              padding: const EdgeInsets.all(16.0),
+              child: SizedBox(
+                width: 200,
+                height: 50,
+                child: ElevatedButton(
+                  style: const ButtonStyle(
+                      backgroundColor:
+                          WidgetStatePropertyAll(Colors.purpleAccent)),
+                  onPressed: () {
+                    Navigator.push(
+                      context,
+                      MaterialPageRoute(builder: (context) => ScreenOne()),
+                    );
+                  },
+                  child: Text(
+                    'Go to Kaleb\'s Screen',
+                    selectionColor: Colors.white,
+                  ),
                 ),
               ),
             ),
-          ),
-        ],
+          ],
+        ),
       ),
     );
   }
